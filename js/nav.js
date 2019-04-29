049766e4000f32fbc6818deb440d8345d691970b
RomSribn <steeper.fest@gmail.com>
2019-04-30 00:04:00 +0300

    initial commit
    basic pages, nav-menu

---
+++ js/nav.js
@@ -0,0 +1,26 @@
+const navs = document.querySelector("nav[role=header-nav]");
+const btn = navs.querySelector("#menu-button");
+const hidMenu = navs.querySelector("ul");
+
+console.log(hidMenu);
+
+btn.addEventListener("click", handleClick);
+
+
+function handleClick() {
+
+    if(!this.classList.contains("open")){
+        this.classList.add("open");
+        hidMenu.classList.add("open");
+        hidMenu.classList.remove("hidden");
+        // hidMenu.style.display = "block";
+
+    }else{
+        this.classList.remove("open");
+        hidMenu.classList.remove("open");
+        hidMenu.classList.add("hidden");
+        // hidMenu.style.display = "";
+    }
+
+
+}
